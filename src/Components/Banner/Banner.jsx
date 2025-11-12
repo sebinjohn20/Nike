4dcb2e05df64484eddd4f885648fc67af9025150
unknown <sebinjohn20@gmail.com>
2025-11-12 16:44:50 +0530

    updated code

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './Banner.css'
 import "../../Fonts/font.css";
@@ -87,30 +87,102 @@ import "../../Fonts/font.css";
     
     
   ]
+   const images = [
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/86de6503-60f3-4057-8801-6c9e5137b164/nike-just-do-it.jpg",
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/5b270984-2818-49e3-b32c-ba5331b84c90/nike-just-do-it.jpg",
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/1ca9b7aa-cd9f-4d2e-93ad-6085270c1fd0/nike-just-do-it.jpg",
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/c4009704-4d0d-454b-a6e3-2aa932128a7c/nike-just-do-it.jpg",
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/2588e343-1440-4c63-9dd4-36c2a551e4e2/nike-just-do-it.jpg",
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/5b270984-2818-49e3-b32c-ba5331b84c90/nike-just-do-it.jpg",
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/c4009704-4d0d-454b-a6e3-2aa932128a7c/nike-just-do-it.jpg",
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/2588e343-1440-4c63-9dd4-36c2a551e4e2/nike-just-do-it.jpg",
+    "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/5b270984-2818-49e3-b32c-ba5331b84c90/nike-just-do-it.jpg",
+
+
+
+
+
+
+
+
+   
+  ];
 function Banner() {
    const navigate=useNavigate()
-    const[currentSlide, setCurrentSlide]=useState(0)
-      const [isPlaying, setIsPlaying] = useState(true);
+   const [currentSlide, setCurrentSlide] = useState(0)
 
-    useEffect(()=>{
-        let intervel;
-        if(isPlaying){
-            intervel=setInterval(() => {
-                setCurrentSlide((prev)=>
-                    prev===bannerData.length-1? 0 : prev+1
-                )
-                
-            }, 5000);
+    const [isPlaying , setIsPlaying]=useState(true)
+    const [progress, setProgress] = useState(0);
+    const silderRef=useRef(null)
+    const sliderRefs = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+   const SLIDE_DURATION = 5000; 
+   const STEP = 100;
+
+useEffect(() => {
+  let progressInterval;
+
+  if (isPlaying) {
+    progressInterval = setInterval(() => {
+      setProgress((prev) => {
+        const next = prev + STEP / SLIDE_DURATION;
+        if (next >= 1) {
+          
+          setCurrentSlide((prevSlide) =>
+            prevSlide === bannerData.length - 1 ? 0 : prevSlide + 1
+          );
+          return 0;
         }
-        return()=>{
-            if(intervel)
-                clearInterval(intervel)
-        }
-    },[isPlaying])
-    const togglePlayPause=()=>{
-        setIsPlaying(!isPlaying)
+        return next;
+      });
+    }, STEP);
+  }
+
+  return () => clearInterval(progressInterval);
+}, [isPlaying]);
+
+const togglePlayPause = () => {
+  setIsPlaying((prev) => !prev);
+};
+const handlePrev = () => {
+  setCurrentSlide((prev) =>
+    prev === 0 ? bannerData.length - 1 : prev - 1
+  );
+  setProgress(0);
+};
+const handleNext = () => {
+  setCurrentSlide((prev) =>
+    prev === bannerData.length - 1 ? 0 : prev + 1
+  );
+  setProgress(0);
+};
+const handleNextSlider=()=>{
+  const silder=silderRef.current
+  if(silder){
+    silder.scrollBy({left:412, behavior: "smooth" })
+  }
+}
+const handlePrevSlider = () => {
+    const slider = silderRef.current;
+    if (slider) {
+      slider.scrollBy({ left: -412, behavior: "smooth" }); 
+      
     }
-    
+  };
+   useEffect(() => {
+    const slider = sliderRefs.current;
+    const handleScroll = () => {
+      const scrollLeft = slider.scrollLeft;
+      const width = slider.clientWidth;
+      const index = Math.round(scrollLeft / width) + 1;
+      setCurrentIndex(index);
+    };
+
+    slider.addEventListener("scroll", handleScroll);
+    return () => slider.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div>
       <div className="banner-container">
@@ -153,36 +225,74 @@ function Banner() {
                   cursor: 'pointer'
                 }}
               ></span>
+              
             ))}
+             </div>
             <div className="pause-stop-btn">
-                <button onClick={togglePlayPause}>
-                    <svg viewBox="0 0 48 48" class="pause-btn-one"><circle cx="24" cy="24" r="22"></circle></svg>
-                    <svg 
-                  viewBox="0 0 48 48" 
-                  className="pause-btn-two"
-                  style={{
-                    strokeDashoffset: isPlaying ? '138.16px' : '0px'
-                  }}
-                >
-                  <circle cx="24" cy="24" r="22"></circle>
-                </svg>
-                   <span className='stop-btn-container'>
-                  {isPlaying ? (
-                 
-                    <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
-                      <rect x="6" y="5" width="4" height="14" fill="currentColor"></rect>
-                      <rect x="14" y="5" width="4" height="14" fill="currentColor"></rect>
-                    </svg>
-                  ) : (
-                    
-                    <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
-                      <path fill="currentColor" fillRule="evenodd" d="M19.314 11.35L6.367 3.877a.75.75 0 00-1.125.65v14.949a.75.75 0 001.125.649l12.947-7.474a.75.75 0 000-1.3z" clipRule="evenodd"></path>
-                    </svg>
-                  )}
-                </span>
-                </button>
+                 <button onClick={togglePlayPause}>
+      
+        <svg viewBox="0 0 48 48" className="pause-btn-one">
+          <circle cx="24" cy="24" r="22"></circle>
+        </svg>
+
+      
+        <svg
+          viewBox="0 0 48 48"
+          className="pause-btn-two"
+          style={{
+            strokeDashoffset: `calc(138.16px * (1 - ${progress}))`,
+          }}
+        >
+          <circle cx="24" cy="24" r="22"></circle>
+        </svg>
+
+    
+        <span className="stop-btn-container">
+          {isPlaying ? (
+      
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              viewBox="0 0 24 24"
+              role="img"
+              width="24px"
+              height="24px"
+              fill="none"
+            >
+              <rect x="6" y="5" width="4" height="14" fill="currentColor"></rect>
+              <rect x="14" y="5" width="4" height="14" fill="currentColor"></rect>
+            </svg>
+          ) : (
+    
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              viewBox="0 0 24 24"
+              role="img"
+              width="24px"
+              height="24px"
+              fill="none"
+            >
+              <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M19.314 11.35L6.367 3.877a.75.75 0 00-1.125.65v14.949a.75.75 0 001.125.649l12.947-7.474a.75.75 0 000-1.3z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          )}
+        </span>
+      </button>
+                <div className="main-silder-arrow-container">
+                  <button className='silder-left-arrow' onClick={handlePrev}>
+                    <svg aria-hidden="true" class="_1ohL1pcH" focusable="false" viewBox="0 0 24 24" role="img" width="20px" height="20px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M15.525 18.966L8.558 12l6.967-6.967"></path></svg>
+                  </button>
+                    <button className='silder-right-arrow' onClick={handleNext}>
+                      <svg aria-hidden="true" class="_1ohL1pcH" focusable="false" viewBox="0 0 24 24" role="img" width="20px" height="20px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M8.474 18.966L15.44 12 8.474 5.033"></path></svg>
+                  </button>
+                </div>
             </div>
-            </div>
+           
             
         </div>
         <div className="featured-banner">
@@ -222,15 +332,15 @@ function Banner() {
                     <div className="silder-shop-text">
                       Shop
                     </div>
-                    <button className='silder-color-arrow-left'>
+                    <button className='silder-color-arrow-left' onClick={handlePrevSlider}>
                       <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M15.525 18.966L8.558 12l6.967-6.967"></path></svg>
                     </button>
-                     <button className='silder-color-arrow-right'>
+                     <button className='silder-color-arrow-right'  onClick={handleNextSlider}>
                       <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M8.474 18.966L15.44 12 8.474 5.033"></path></svg>
                     </button>
                   </div>
                 </div>
-                <ul className="triple-silder-container">
+                <ul className="triple-silder-container" ref={silderRef}>
                   <li>
                     <div className="product-silde-container">
                         <div className="product-silder-flex-container">
@@ -352,11 +462,22 @@ function Banner() {
             <div className="shop-bysport-container-body">
                 <div className="shop-bysport-container-body-top">
                     <div className="shop-by-sport-text-container">
-                        <h2>Shop by Sport</h2>
+                      <div>  <h2>Shop by Sport</h2></div>
+
+                      <div className="shop-silder-arrow-container">
+                    
+                    <button className='silder-color-arrow-left' onClick={handlePrevSlider}>
+                      <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M15.525 18.966L8.558 12l6.967-6.967"></path></svg>
+                    </button>
+                     <button className='silder-color-arrow-right'  onClick={handleNextSlider}>
+                      <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M8.474 18.966L15.44 12 8.474 5.033"></path></svg>
+                    </button>
+                  </div>
+
                     </div>
                     <ul className="shop-bysport-silder-container">
                       {sportData.map((item,index)=>(
-                        <li key={index}>
+                        <li key={index} >
                             <div className='shop-bysport-silder-img-co'>
                                 <img src={item.img} alt="" />
                             </div>
@@ -377,11 +498,19 @@ function Banner() {
                 <div className="shop-by-icon-text-container">
                   <h2>Shop by Icons</h2>
                 </div>
-                <div className="shop-by-icon-silder-container">
-                 <span className='silder-count'>1/9</span>
-                 <img src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/h_375,c_limit/86de6503-60f3-4057-8801-6c9e5137b164/nike-just-do-it.jpg" alt="" />
-                 
+                    <div className="shop-by-icon-slider">
+                <div className="shop-by-icon-silder-container" ref={sliderRefs}>
+                  <span className="silder-count">
+                    {currentIndex}/{images.length}
+                  </span>
+
+                  {images.map((img, i) => (
+                    <img key={i} src={img} alt={`Slide ${i + 1}`} />
+                  ))}
                 </div>
+              </div>
+
+                
               </div>
             </div>
         </div>
